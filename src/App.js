ba7c0e925fc546e7b8ee80b235c035b1f58a1c54
dimusiq <dumojke@gmail.com>
2022-04-07 23:22:26 +0500

    added feedback  item and card component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/header/header.component';
+import FeedbackList from './components/feedback-list/feedback-list.component';
+import FeedbackData from './data/data';
 
 
 function App() {
+    const [feedback, setFeedback] = useState(FeedbackData)
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to delete?'))
+        setFeedback(feedback.filter((item)=> item.id !== id))
+    }
     return (
         <>
         <Header />
             <div className='container'>
-                <h1>My App</h1>
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
             </div>
         </>
     )
---
+++ src/components/card/card.styles.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const CardStyles = styled.div`
+  background-color: #fff;
+  color: #333;
+  border-radius: 15px;
+  padding: 40px 50px;
+  margin: 20px 0;
+  position: relative;
+`
+export const CardReverse = styled.div`
+    background-color: rgba(0, 0, 0, 0.4);
+    color: #fff;
+`
+export const DisplayNumbers = styled.div`
+    position: absolute;
+    top: -10px;
+    left: -10px;
+`
+export const TextDisplay = styled.div`
+
+`
